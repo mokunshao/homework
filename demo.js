6af5076356ce43a9b2de5da98d64210a8258ce27
mokunshao <1627510957@qq.com>
2018-07-17 01:12:38 +0800

    minor change

--- demo.js
+++ demo.js
@@ -1,18 +1,18 @@
- var fs = require('fs');
- var dirName = process.argv[2]; 
+var fs = require('fs');
+var dirName = process.argv[2]; 
 try{
-	fs.accessSync(dirName);
-	process.exit(1);
+    fs.accessSync(dirName);
+    process.exit(1);
 }catch(e){
-	 fs.mkdirSync("./" + dirName);
-	 process.chdir("./" + dirName);
-	 fs.mkdirSync('css');
-	 fs.mkdirSync('js'); 
-	 fs.writeFileSync("./index.html", "<!DOCTYPE>");
-	 fs.appendFileSync("./index.html", "\n"+"<title>Hello</title>");
-	 fs.appendFileSync("./index.html", "\n"+"<h1>Hi</h1>");
-	 fs.writeFileSync("css/style.css", "h1{color: red;}");
-	 fs.writeFileSync("./js/main.js", 'var string = "Hello World"');
-	 fs.appendFileSync("./js/main.js", "\n"+"alert(string)");
-	 process.exit(0);
+    fs.mkdirSync("./" + dirName);
+    process.chdir("./" + dirName);
+    fs.mkdirSync('css');
+    fs.mkdirSync('js'); 
+    fs.writeFileSync("./index.html", "<!DOCTYPE>");
+    fs.appendFileSync("./index.html", "\n"+"<title>Hello</title>");
+    fs.appendFileSync("./index.html", "\n"+"<h1>Hi</h1>");
+    fs.writeFileSync("css/style.css", "h1{color: red;}");
+    fs.writeFileSync("./js/main.js", 'var string = "Hello World"');
+    fs.appendFileSync("./js/main.js", "\n"+"alert(string)");
+    process.exit(0);
 }
